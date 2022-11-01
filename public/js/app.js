4db83f39fbba63c822fa6106fbed204e55bc6ea1
Seth Atam <seth.atam@gmail.com>
2022-11-01 12:37:32 +0100

    Updated Settings

--- public/js/app.js
+++ public/js/app.js
@@ -25936,6 +25936,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _utils_FormData__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ../../../utils/FormData */ "./resources/js/utils/FormData.js");
 /* harmony import */ var _utils_Payment__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ../../../utils/Payment */ "./resources/js/utils/Payment.js");
 /* harmony import */ var _Forms_Select__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ../../Forms/Select */ "./resources/js/components/Forms/Select.vue");
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 
 
 
@@ -26010,7 +26022,7 @@ __webpack_require__.r(__webpack_exports__);
         currency: "NGN",
         first_name: props.user.name,
         metadata: {
-          custom_fields: [form]
+          custom_fields: _toConsumableArray(form)
         },
         callback: function callback(response) {
           console.log(response);
