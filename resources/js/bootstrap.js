716bf97e5833047df3af1eae82f7696fc29fd095
Seth Atam <seth.atam@gmail.com>
2022-11-09 12:27:52 +0100

    Updated Ssl

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -33,7 +33,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: false,
+    forceTLS: true,
     wsHost: process.env.MIX_PUSHER_HOST,
     wsPort: 6001,
     disableStats: true,
