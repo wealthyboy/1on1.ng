1d55db4390bcd7ce445ccde3203866d983b34dd5
Seth Atam <seth.atam@gmail.com>
2022-11-15 05:17:20 +0100

    Updated Webhook

--- app/Http/Controllers/WebHook/WebHookController.php
+++ app/Http/Controllers/WebHook/WebHookController.php
@@ -46,41 +46,41 @@ class WebHookController extends Controller
             // Log::info($request->all());
             Log::info($request->data['metadata']['custom_fields']['form']);
 
-            // $input =  $request->data['metadata']['custom_fields'][0];
+            $input =  $request->data['metadata']['custom_fields']['form'];
 
-            // if ($input['service_type'] == 'shout-out') {
-            //     $shout_out = new ShoutOut;
-            //     $shout_out->recipient_first_name = $input['sender_last_name'];
-            //     $shout_out->recipient_last_name = $input['sender_last_name'];
-            //     $shout_out->recipient_email = $input['recipient_email'];
-            //     $shout_out->recipient_phone = $input['recipient_phone'];
-            //     $shout_out->service_id = $input['service_id'];
-            //     $shout_out->user_id = $input['user_id'];
-            //     $shout_out->type = $input['type'];
-            //     //$shout_out->event_date = $input['event_date'];
-            //     $shout_out->comment = $input['comment'];
-            //     $shout_out->date = $input['date'];
-            //     $shout_out->save();
+            if ($input['service_type'] == 'shout-out') {
+                $shout_out = new ShoutOut;
+                $shout_out->recipient_first_name = $input['sender_last_name'];
+                $shout_out->recipient_last_name = $input['sender_last_name'];
+                $shout_out->recipient_email = $input['recipient_email'];
+                $shout_out->recipient_phone = $input['recipient_phone'];
+                $shout_out->service_id = $input['service_id'];
+                $shout_out->user_id = $input['user_id'];
+                $shout_out->type = $input['type'];
+                //$shout_out->event_date = $input['event_date'];
+                $shout_out->comment = $input['comment'];
+                $shout_out->date = $input['date'];
+                $shout_out->save();
 
-            //     //Mail::to();
-            // }
+                //Mail::to();
+            }
 
 
-            // if ($input['service_type'] == 'master-class') {
-            //     $shout_out = new ShoutOut;
-            //     $shout_out->recipient_first_name = $input['sender_last_name'];
-            //     $shout_out->recipient_last_name = $input['sender_last_name'];
-            //     $shout_out->recipient_email = $input['recipient_email'];
-            //     $shout_out->recipient_phone = $input['recipient_phone'];
-            //     $shout_out->service_id = $input['service_id'];
-            //     $shout_out->type = $input['type'];
-            //     //$shout_out->event_date = $input['event_date'];
-            //     $shout_out->comment = $input['comment'];
-            //     $shout_out->date = $input['date'];
-            //     $shout_out->save();
+            if ($input['service_type'] == 'master-class') {
+                $shout_out = new ShoutOut;
+                $shout_out->recipient_first_name = $input['sender_last_name'];
+                $shout_out->recipient_last_name = $input['sender_last_name'];
+                $shout_out->recipient_email = $input['recipient_email'];
+                $shout_out->recipient_phone = $input['recipient_phone'];
+                $shout_out->service_id = $input['service_id'];
+                $shout_out->type = $input['type'];
+                //$shout_out->event_date = $input['event_date'];
+                $shout_out->comment = $input['comment'];
+                $shout_out->date = $input['date'];
+                $shout_out->save();
 
-            //     //Mail::to();
-            // }
+                //Mail::to();
+            }
         } catch (\Throwable $th) {
             Log::info("Custom error :" . $th);
         }
