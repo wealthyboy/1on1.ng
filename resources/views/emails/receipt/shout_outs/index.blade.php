76d59326d392b4611a915bd09e064f410d14231b
Seth Atam <seth.atam@gmail.com>
2022-11-18 07:34:29 +0100

    Updated wallet

--- resources/views/emails/receipt/shout_outs/index.blade.php
+++ resources/views/emails/receipt/shout_outs/index.blade.php
@@ -430,7 +430,7 @@
                                                    <tr>
                                                       <td class="wz" width="30"></td>
                                                       <td valign="top" align="center">
-                                                         <table class="tableTxt" width="252" cellspacing="0" cellpadding="0" border="0" align="left">
+                                                         <table class="tableTxt" width="400" cellspacing="0" cellpadding="0" border="0" align="left">
 
                                                             <tr>
                                                                <td class="RegularText4TD" data-link-style="text-decoration:none; color:#67bffd;" data-link-color="SectionInfoLink" data-color="SectionInfoTXT" style="color: #425065;font-family: sans-serif;font-size: 18px;font-weight: bold;text-align: left;line-height: 23px;" width="179" valign="top" align="left"><a href="#" target="_blank" style="text-decoration: none;color: #67bffd;font-weight: bold;" data-color="SectionInfoLink"></a>Details</td>
