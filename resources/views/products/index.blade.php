31d678f4cb4688a5bd151e5389c349e0d5da28c1
Seth Atam <seth.atam@gmail.com>
2022-12-02 19:08:37 +0100

    Updated

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,7 @@
 <section class="py-5">
     <div class="container">
 
-        <div class="row">
+        <div class="row mt-5">
             <div class="col-lg-6">
                 <h2 class="mc-text-h2 mc-my-2 d-inline-block">Trending {{ $category->name }}</h2>
                 <span>sll</span>
