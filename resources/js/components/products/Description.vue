e110a987c8a0e6b00f2eb5039e65f5916cda626b
Jacob <jacob.atam@gmail.com>
2023-10-01 03:43:03 -0700

    Fix

--- resources/js/components/products/Description.vue
+++ resources/js/components/products/Description.vue
@@ -1,23 +1,17 @@
 <template>
-
   <div class="row border-top mt-5">
     <div class="col-md-6">
 
       <h1 class="mt-3">Description</h1>
-      <p v-html="description"></p>
+      <p class="text-secondary" v-html="description"></p>
     </div>
 
-    <div
-      id="#sch"
-      v-if="schedules"
-      class="col-md-6 mt-3"
-    >
+    <div id="#sch" v-if="schedules" class="col-md-6 mt-3">
       <h1>Schedule</h1>
-      <ul class="li">
-        <li
-          v-for="schedule in schedules"
-          :key="schedule.id"
-        > Date: {{ schedule.starts_at }} - {{ schedule.ends_at }} | Time: {{ schedule.start_time}} - {{ schedule.end_time }}</li>
+      <ul class="li text-secondary">
+        <li class="text-secondary" v-for="schedule in schedules" :key="schedule.id"> Date: {{ schedule.starts_at }} - {{
+          schedule.ends_at }} |
+          Time: {{ schedule.start_time }} - {{ schedule.end_time }}</li>
 
       </ul>
     </div>
@@ -26,6 +20,6 @@
 <script>
 export default {
   props: ["description", "schedules"],
-  setup() {},
+  setup() { },
 };
 </script>
